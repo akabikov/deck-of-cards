4e8aecde28399be1b0353909f7d2e3a23b1e3b85
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-05-02 16:10:18 +0300

    Updates

---
+++ precache-manifest.b9c9798edf33e6801a86b1fee57f5743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8cc213d854b17dac1e46079a667f28",
+    "url": "/deck-of-cards/index.html"
+  },
+  {
+    "revision": "9c06f61fbc83690d9549",
+    "url": "/deck-of-cards/static/css/main.6f694e81.chunk.css"
+  },
+  {
+    "revision": "1172a84f249356c33cbe",
+    "url": "/deck-of-cards/static/js/2.4376dbd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deck-of-cards/static/js/2.4376dbd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c06f61fbc83690d9549",
+    "url": "/deck-of-cards/static/js/main.ddef63e4.chunk.js"
+  },
+  {
+    "revision": "2fd85d6a0e2ff0169348",
+    "url": "/deck-of-cards/static/js/runtime-main.02baa986.js"
+  }
+]);
